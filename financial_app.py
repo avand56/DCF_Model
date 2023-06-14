7ece10e55d6a76dc8afb207739fa4169ad0cb8aa
Alex Vanderhoeff <alexvanderhoeff@Alexs-Air.attlocal.net>
2023-06-14 14:47:33 -0400

    f

--- financial_app.py
+++ financial_app.py
@@ -10,29 +10,28 @@ app = Flask(__name__)
 
 @app.route('/stocks', methods = ['POST', 'GET'])
 def cb():
-    return gm(request.args.get('data'), float(request.args.get('growth_rate')))
+    return gm(request.args.get('data'), float(request.args.get('growth_rate')), float(request.args.get('terminal_growth')), float(request.args.get('iterations')), float(request.args.get('risk_free_rate')), float(request.args.get('beta')), float(request.args.get('market_rate_return')))
 
     
 @app.route('/')
 def index():
     return render_template('charts.html')
 
-def gm(symbol="MSFT", growth_rate = 0.10):
+def gm(symbol="MSFT", growth_rate = 0.10, terminal_growth = 0.04, iterations = 10000.0,  risk_free_rate = 0.04, beta = 1.00, market_rate_return = 0.08):
 
     output_distribution=run_mcs(
         symbol, 
         growth_rate, 
-        0.04, 
-        10000, 
-        0.04, 
-        1.00,
-        0.08
+        terminal_growth,
+        iterations, 
+        risk_free_rate, 
+        beta,
+        market_rate_return
         )
   
     # Create a histogram
     fig = px.histogram(output_distribution,
-        nbins=50, template="seaborn", x="value", y="count", 
-        labels=dict(value="Market Cap ($)", count="Simulation Outcomes"))
+        nbins=50, template="seaborn")
 
     # Create a JSON representation of the graph
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
