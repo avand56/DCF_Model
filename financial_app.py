6848d5723f69c8d4f38041d1161193abb817511e
Alex Vanderhoeff <alexvanderhoeff@Alexs-Air.attlocal.net>
2023-06-14 15:42:49 -0400

    f

--- financial_app.py
+++ financial_app.py
@@ -10,23 +10,23 @@ app = Flask(__name__)
 
 @app.route('/stocks', methods = ['POST', 'GET'])
 def cb():
-    return gm(request.args.get('data'), float(request.args.get('growth_rate'))) #, float(request.args.get('terminal_growth')), float(request.args.get('iterations')), float(request.args.get('risk_free_rate')), float(request.args.get('beta')), float(request.args.get('market_rate_return')))
+    return gm(request.args.get('data'), float(request.args.get('growth_rate')), float(request.args.get('terminal_growth')), int(request.args.get('iterations')), float(request.args.get('risk_free_rate')), float(request.args.get('beta')), float(request.args.get('market_rate_return')))
 
     
 @app.route('/')
 def index():
     return render_template('charts.html')
 
-def gm(symbol="MSFT", growth_rate = 0.10):#, terminal_growth = 0.04, iterations = 10000.0,  risk_free_rate = 0.04, beta = 1.00, market_rate_return = 0.08):
+def gm(symbol="MSFT", growth_rate = 0.10, terminal_growth = 0.04, iterations = 10000,  risk_free_rate = 0.04, beta = 1.00, market_rate_return = 0.08):
 
     output_distribution=run_mcs(
         symbol, 
         growth_rate, 
-        0.04,
-        10000,
-        0.04,
-        1.0,
-        0.08
+        terminal_growth,
+        iterations,
+        risk_free_rate,
+        beta,
+        market_rate_return
         )
   
     # Create a histogram
